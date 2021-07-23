0b6fccfb7820cd9eec507aff8eb15e762525f9bb
TL <TL@gmail.com>
2021-07-23 09:36:23 +0530

    initial commit

---
+++ GIT_TL/DemoProj/src/com/dil/beans/Util.java
@@ -0,0 +1,10 @@
+package com.dil.beans;
+
+import java.util.Date;
+
+public class Util {
+
+	public Date getSysDate() {
+		return new Date();
+	}
+}
---
+++ GIT_TL/DemoProj/src/com/dil/test/Test.java
@@ -0,0 +1,5 @@
+package com.dil.test;
+
+public class Test {
+
+}
